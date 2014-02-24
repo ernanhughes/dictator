b49e81fef63f8b7016aa5dc156ba516f03bd07fd
Ernan Hughes <ernanhughes@gmail.com>
2014-02-24 09:51:44 +0000

    added a timer, a creat for large buttons and a good start on the large buttons.

--- Dictator/src/main/java/com/banba/dictator/MainActivity.java
+++ Dictator/src/main/java/com/banba/dictator/MainActivity.java
@@ -3,8 +3,16 @@ package com.banba.dictator;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,6 +24,9 @@ import android.widget.ShareActionProvider;
 
 import com.banba.dictator.event.SectionEvent;
 import com.banba.dictator.ui.ColorUtil;
+import com.banba.dictator.ui.L;
+
+import java.io.File;
 
 import de.greenrobot.event.EventBus;
 
@@ -65,10 +76,8 @@ public class MainActivity extends Activity {
             Intent i = new Intent(this, SearchActivity.class);
             startActivity(i);
         } else if (SectionEvent.RECORD.equalsIgnoreCase(section.section)) {
-            Intent i = new Intent(this, RecordingActivity.class);
-            startActivity(i);
-        } else if (SectionEvent.MANAGE.equalsIgnoreCase(section.section)) {
-            // manage of entries
+
+            startRecording();
         } else if (SectionEvent.MANAGE.equalsIgnoreCase(section.section)) {
             // manage of entries
         } else if (SectionEvent.ENTRIES.equalsIgnoreCase(section.section)) {
@@ -81,6 +90,70 @@ public class MainActivity extends Activity {
             startActivity(i);
         }
     }
+
+    MediaRecorder mRecorder;
+    String mOutputFile;
+    File mSampleFile = null;
+
+    protected void startRecording() {
+        mIsRecording = true;
+        mUpdateTimeTask.run();
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        try {
+            File outputDir = getCacheDir();
+            File outputFile = File.createTempFile("prefix", "extension", outputDir);
+            mOutputFile = outputFile.getAbsolutePath();
+            mRecorder.setOutputFile(mOutputFile);
+            mRecorder.prepare();
+        } catch (Exception ex) {
+            L.e(ex.getMessage());
+        }
+        mRecorder.start();
+        if (this.mSampleFile == null) {
+            File sampleDir = Environment.getExternalStorageDirectory();
+            try {
+                mSampleFile = File.createTempFile("Recording",
+                        ".ogg", sampleDir);
+            } catch (Exception e) {
+                L.e("Access error " + e.getMessage());
+                return;
+            }
+        }
+    }
+
+    protected void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        ContentValues values = new ContentValues(3);
+        long current = System.currentTimeMillis();
+        values.put(MediaStore.MediaColumns.TITLE, "test_audio");
+        values.put(MediaStore.MediaColumns.DATE_ADDED, (int) (current / 1000));
+        values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/3gpp");
+        values.put(MediaStore.MediaColumns.DATA, mOutputFile);
+        ContentResolver contentResolver = getContentResolver();
+        Uri base = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Uri newUri = contentResolver.insert(base, values);
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, newUri));
+    }
+
+
+    boolean mIsRecording = false;
+    long mLastLogTime, mTotalTime = 0l;
+    Handler mHandler = new Handler();
+    private Runnable mUpdateTimeTask = new Runnable() {
+        public void run() {
+            if (mIsRecording) {
+                long now = SystemClock.uptimeMillis();
+                mTotalTime += now - mLastLogTime;
+                mLastLogTime = now;
+                setTitle("Recording  " + TimeFormatter.formatTime(mTotalTime / 1000));
+                mHandler.postDelayed(mUpdateTimeTask, 1000);
+            }
+        }
+    };
 
 
     public static class MainFragment extends Fragment {
