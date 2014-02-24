f5761d7292ead91ed536a08a44d0c9f8b0d02281
Ernan Hughes <ernanhughes@gmail.com>
2014-02-24 11:56:10 +0000

    integrated table fixed headers. added better icons, fixed the display of the titles.

--- Dictator/src/main/java/com/banba/dictator/MainActivity.java
+++ Dictator/src/main/java/com/banba/dictator/MainActivity.java
@@ -2,10 +2,10 @@ package com.banba.dictator;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,14 +13,16 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.provider.MediaStore;
-import android.view.LayoutInflater;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ShareActionProvider;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.banba.dictator.event.SectionEvent;
 import com.banba.dictator.ui.ColorUtil;
@@ -37,21 +39,29 @@ import de.greenrobot.event.EventBus;
 public class MainActivity extends Activity {
     private ShareActionProvider mShareActionProvider;
 
+    MediaRecorder mRecorder;
+    String mOutputFile;
+    File mSampleFile = null;
+    ImageButton mRecordButton;
+    TextView mRecordText;
+    boolean mIsRecording = false;
+    long mLastLogTime = 0l, mTotalTime = 0l;
+    Handler mHandler = new Handler();
+    int oldActionBarColor = Color.BLACK;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
+        setContentView(R.layout.activity_main);
         final ActionBar actionBar = getActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeButtonEnabled(true);
         }
+        mRecordButton = (ImageButton) findViewById(R.id.iconRecord);
+        mRecordText = (TextView) findViewById(R.id.textRecord);
+
         EventBus.getDefault().register(this);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new MainFragment())
-                    .commit();
-        }
     }
 
     @Override
@@ -76,8 +86,11 @@ public class MainActivity extends Activity {
             Intent i = new Intent(this, SearchActivity.class);
             startActivity(i);
         } else if (SectionEvent.RECORD.equalsIgnoreCase(section.section)) {
-
-            startRecording();
+            if (mIsRecording) {
+                stopRecording();
+            } else {
+                startRecording();
+            }
         } else if (SectionEvent.MANAGE.equalsIgnoreCase(section.section)) {
             // manage of entries
         } else if (SectionEvent.ENTRIES.equalsIgnoreCase(section.section)) {
@@ -91,20 +104,18 @@ public class MainActivity extends Activity {
         }
     }
 
-    MediaRecorder mRecorder;
-    String mOutputFile;
-    File mSampleFile = null;
 
     protected void startRecording() {
         mIsRecording = true;
         mUpdateTimeTask.run();
+        mRecordButton.setImageResource(R.drawable.record_stop_large);
         mRecorder = new MediaRecorder();
         mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
         try {
             File outputDir = getCacheDir();
-            File outputFile = File.createTempFile("prefix", "extension", outputDir);
+            File outputFile = File.createTempFile("prefix", ".3gpp", outputDir);
             mOutputFile = outputFile.getAbsolutePath();
             mRecorder.setOutputFile(mOutputFile);
             mRecorder.prepare();
@@ -125,14 +136,23 @@ public class MainActivity extends Activity {
     }
 
     protected void stopRecording() {
+        mIsRecording = false;
+        mTotalTime = 0l;
         mRecorder.stop();
         mRecorder.release();
-        ContentValues values = new ContentValues(3);
+        mRecordButton.setImageResource(R.drawable.record_start_large);
+        getActionBar().setTitle(R.string.app_name);
+        mRecordText.setText("RECORD");
+
+        ContentValues values = new ContentValues();
         long current = System.currentTimeMillis();
         values.put(MediaStore.MediaColumns.TITLE, "test_audio");
         values.put(MediaStore.MediaColumns.DATE_ADDED, (int) (current / 1000));
         values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/3gpp");
         values.put(MediaStore.MediaColumns.DATA, mOutputFile);
+
+        Toast.makeText(this, "File saved: " + mOutputFile, Toast.LENGTH_LONG).show();
+
         ContentResolver contentResolver = getContentResolver();
         Uri base = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         Uri newUri = contentResolver.insert(base, values);
@@ -140,27 +160,20 @@ public class MainActivity extends Activity {
     }
 
 
-    boolean mIsRecording = false;
-    long mLastLogTime, mTotalTime = 0l;
-    Handler mHandler = new Handler();
     private Runnable mUpdateTimeTask = new Runnable() {
         public void run() {
             if (mIsRecording) {
+                if (mLastLogTime == 0l) {
+                    mLastLogTime = SystemClock.uptimeMillis();
+                }
                 long now = SystemClock.uptimeMillis();
                 mTotalTime += now - mLastLogTime;
                 mLastLogTime = now;
-                setTitle("Recording  " + TimeFormatter.formatTime(mTotalTime / 1000));
+                Spanned text = Html.fromHtml("<font color=\"#800000\">Recording " + TimeFormatter.formatTime(mTotalTime / 1000) + "</font>");
+                getActionBar().setTitle(text);
+                mRecordText.setText(text);
                 mHandler.postDelayed(mUpdateTimeTask, 1000);
             }
         }
     };
-
-
-    public static class MainFragment extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_main, container, false);
-        }
-    }
 }
